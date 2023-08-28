42a72632712c6cbe63179925ca82f173a96d8519
EungBug <eungb14@gmail.com>
2023-08-28 20:01:36 +0900

    feat: REST API 작성하기 연습

--- src/index.js
+++ src/index.js
@@ -5,34 +5,70 @@ import dayjs from 'dayjs';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
+let users = [
+  {
+    id: 1,
+    name: 'Eun bi',
+    age: 18
+  }
+];
+
+// express 앱 생성
 const app = express();
 
-app.use(
-  cors({
-    // 특정 도메인으로 요청이 와야지만 허용
-    origin: 'https://www.google.com'
-  })
-);
+// 미들웨어
+app.use(cors());
 app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true, limit: '700mb' }));
 
-const today = new Date();
-const todayjs = dayjs(today).format('YYYY-MM-DD');
-console.log(`today: ${todayjs}`);
-
-// bcrypt
-const password = '1234';
-const hashedPassword = bcrypt.hashSync(password, 10); // 암호화 된 패스워드 생성
-console.log(hashedPassword);
-
-// jwt
-const token = jwt.sign('1234', 'aasdfewrfdsa');
-
-console.log(token);
-
-app.get('/', (req, res) => {
-  res.send('Hello, Node.js');
+// GET - 유저 조회
+// query or path
+// 성공 status : 200
+app.get('/users', (req, res) => {
+  res.status(200).json({ users });
 });
 
+// POST - 유저 생성
+// 요청 -> body
+// 성공 status : 201
+app.post('/users', (req, res) => {
+  const { name, age } = req.body;
+  users.push({
+    id: new Date().getTime(),
+    name,
+    age
+  });
+
+  res.status(201).json({ users });
+});
+
+// PATCH - 유저 정보 수정
+// :id : path variable => req.params.id
+// 성공 status : 204
+app.patch('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, age } = req.body;
+  const targetUserIndex = users.findIndex(user => user.id === Number(id));
+  users[targetUserIndex] = {
+    id: users[targetUserIndex].id,
+    name: name ?? users[targetUserIndex].name,
+    age: age ?? users[targetUserIndex].age
+  };
+
+  res.status(204).json({});
+});
+
+// DELETE - 유저 삭제
+// 성공 status: 204
+app.delete('/users/:id', (req, res) => {
+  const { id } = req.params;
+  const deletedUsers = users.filter(user => user.id !== Number(id));
+  users = deletedUsers;
+  res.status(204).json({});
+});
+
+// express 앱 실행하기
 app.listen(8000, () => {
   console.log('서버가 시작되었습니다.');
 });
